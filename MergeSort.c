7456695a88f0bdfdb9b8aadd4682a02a20dff62e
ANTONIO COSTA SATIRO DE SOUZA <10723636@mackenzista.com.br>
2025-11-11 00:10:30 +0000

    Ajustes mergesort

--- MergeSort.c
+++ MergeSort.c
@@ -1,20 +1,10 @@
 // Nome: Antonio Costa Satiro de Souza Ra: 10723636
 
 #include <stdio.h>
-#define TAM_MAX_VETOR = 100000
-int AuxiliarMerge[];
+#define TAM_MAX_VETOR 100000
+int AuxiliarMerge[TAM_MAX_VETOR];
 
-
-void mergeSort(int vetor[],int esquerda, int direita){
-
-    int meio = (direita + esquerda) / 2;
-    if (esquerda >= direita) return;
-    mergeSort(vetor, esquerda, meio);
-    mergeSort(vetor, meio + 1, direita);
-    merge(vetor, esquerda, meio, direita);
-}
-
-void merge(char vetor[], int esquerda, int meio, int direita){
+void merge(int vetor[], int esquerda, int meio, int direita){
     
     int i, j, k;
 
@@ -27,10 +17,19 @@ void merge(char vetor[], int esquerda, int meio, int direita){
     }
 }
 
-int maior(int vetor[]){
+void mergeSort(int vetor[],int esquerda, int direita){
+
+    int meio = (direita + esquerda) / 2;
+    if (esquerda >= direita) return;
+    mergeSort(vetor, esquerda, meio);
+    mergeSort(vetor, meio + 1, direita);
+    merge(vetor, esquerda, meio, direita);
+}
+
+int maior(int vetor[], int tam){
 
     int aux = vetor[0];
-    for(int i=0; i < tamanho(vetor); i++){
+    for(int i=0; i < tam; i++){
         if(vetor[i] > aux){
             aux = vetor[i];
         }
@@ -40,8 +39,17 @@ int maior(int vetor[]){
 
 }
 
-int tamanho(int vetor[]){
+int main(void) {
+    int array [] = {14, 2, 6, 8, 5, 4, 3, 1, 7, 9, 6, 10, 17, 20, 12, 19};
+    int n = sizeof(array) / sizeof(array[0]);
+    mergeSort(array, 0, n -1);
 
-    int tam = (sizeof(vetor) / sizeof(vetor[0]));
-    return tam;
+    printf("[");
+    for(int i =0; i < n ; i++ ){
+        printf("%d", array[i]);
+        if(i < n-1){
+            printf(", ");
+        }
+    }
+    printf("]");
 }
